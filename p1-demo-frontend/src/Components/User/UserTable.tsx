6446c72378a8de0b8cc0c45c7a4f403a7c75fed6
Timothy Ku <timothykukt@gmail.com>
2025-02-18 09:23:00 -0600

    add a mock function to the button

--- p1-demo-frontend/src/Components/User/UserTable.tsx
+++ p1-demo-frontend/src/Components/User/UserTable.tsx
@@ -34,6 +34,11 @@ export const UserTable:React.FC = () => {
         } 
     }
 
+    //function that does a fake update delete 9wanna show how to extract data from a map)
+    const updateUser = (user:User) => {
+        alert("User " + user.userId + " has been fake updated or deleted")
+    }
+
     return (
         <Container className="d-flex flex-column align-items-center">
 
@@ -56,8 +61,8 @@ export const UserTable:React.FC = () => {
                             <td>{user.username}</td>
                             <td>{user.role}</td>
                             <td>
-                                <Button variant="outline-success">Promote</Button>
-                                <Button variant="outline-danger">Fire</Button>
+                                <Button variant="outline-success" onClick={() => updateUser(user)}>Promote</Button>
+                                <Button variant="outline-danger" onClick={() => updateUser(user)}>Fire</Button>
                             </td>
                         </tr>
                     ))} {/* WHY () to open the arrow func? bc it implicityly returns (i.e. no need the return keyword) */}
